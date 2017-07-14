97188e20e2565dabe0a18a96b93369405ac46acd
Fabrice Carrega <fabrice.carrega@treize37.com>
2017-07-14 17:29:34 +0200

    Corrected financial statements loading process

--- 2pm/database.py
+++ 2pm/database.py
@@ -1,7 +1,7 @@
 import csv
 from tinydb import TinyDB
 
-
+# Return given database name
 def get(name):
     return TinyDB('db/' + name + '.json')
 
@@ -10,11 +10,26 @@ def load_financial_statement(ticker, statement, frequency):
     filename = 'data/' + ticker + '/' + ticker + statement + frequency + '.csv'
     with open(filename, newline = '') as csvfile:
         reader = csv.reader(csvfile)
-        for row in reader:
-            print(', '.join(row))
+        dates = next(reader, None)                  # Retrieve dates on file header
+        for row in reader:                          # Loop on each line
+            key = row[0].strip()                    # Keep track of keys
+            if not empty_row(row):                  # Only insert
+                for index, value in enumerate(row[1:len(row)-1]):
+                    string_to_output = key + ": "
+                    string_to_output += dates[index] + ': '
+                    string_to_output += value + ' | '
+                    print(string_to_output)
 
 # Load Seeking Alpha financial statements files for a given ticker
 def load_financial_statements(ticker):
     for statement in ['BalanceSheet', 'CashFlowStatement', 'IncomeStatement']:
         for frequency in ['Annual', 'Quarterly']:
-            load_financial_statement(ticker, type, statement, frequency)
+            load_financial_statement(ticker, statement, frequency)
+
+# Checks if a financial statement row is emtpy (title)
+def empty_row(row):
+    empty_row = False
+    for element in row[1:len(row)-1]:
+        if element == "":
+            empty_row = empty_row or True
+    return empty_row
